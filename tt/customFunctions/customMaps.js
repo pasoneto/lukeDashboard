a466f2616edf7f7ace1be5e101df274fa1e9589e
Pedro Neto <pasoneto@gmail.com>
2023-02-15 18:47:13 +0200

    add info graphs

--- tt/customFunctions/customMaps.js
+++ tt/customFunctions/customMaps.js
@@ -81,44 +81,149 @@ function styleGen(feature){
               weight: 1}
     }
 }
+function getColor(d, quantList) {
+    if(d <= quantList[0]){
+      return("#DAF7A6")
+    }
+    if(d <= quantList[1] && d > quantList[0]){
+      return("#FFC300")
+    }
+    if(d <= quantList[2] && d > quantList[1]){
+      return("#FF5733")
+    }
+    if(d <= quantList[3] && d > quantList[2]){
+      return("#C70039")
+    }
+    if(d <= quantList[4] && d > quantList[3]){
+      return("#900C3F")
+    }
+    if(d <= quantList[5] && d > quantList[4]){
+      return("#581845")
+    }
+}
+
+const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
+
+// sort array ascending
+const asc = arr => arr.sort((a, b) => a - b);
+const sum = arr => arr.reduce((a, b) => a + b, 0);
+const mean = arr => sum(arr) / arr.length;
+
+// sample standard deviation
+const std = (arr) => {
+    const mu = mean(arr);
+    const diffArr = arr.map(a => (a - mu) ** 2);
+    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+};
+
+const quantile = (arr, q) => {
+    const sorted = asc(arr);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+};
+
+function styleGenColors(feature){
+    var properties = feature.properties
+    if(properties){
+      var regionCode = properties[0][regionDivision]
+      var selectedRegions = window.checkedValues[regionDivision]
+      var regionWasSelected = selectedRegions.indexOf(regionCode.toString()) !== -1
+
+      //Setting up colors for each region
+      var currentRegionProperties = filteredData.filter(i => i[regionDivision] == regionCode)
+      var currentRegionValues = currentRegionProperties.map(i => i.value)
+      var currentRegionValues = currentRegionValues.filter(Number) //Removing null values
+      
+      var allValues = filteredData.map(i => i.value) //Getting all values for all regions selected
+      var allValues = allValues.filter(Number) //Filtering nulls
+      
+      var quantilesList = [0.2, 0.4, 0.6, 0.7, 0.8] //Sets the boundaries in terms of 7 quantiles
+      var quantilesCalculated = quantilesList.map(i => quantile(allValues, i))
+
+      var currentValueReg = arrAvg(currentRegionValues)
+      
+      var quantilesList = [0, 0.2, 0.4, 0.6, 0.7, 0.8] //Sets the boundaries in terms of 7 quantiles
+      window.legend = L.control({position: 'bottomright'});
+      legend.onAdd = function (map) {
+          var div = L.DomUtil.create('div', 'info legend')
+          var grades = ["#DAF7A6", "#FFC300", "#FF5733", "#C70039", "#900C3F"]
+          // loop through our density intervals and generate a label with a colored square for each interval
+          for(var i=0; i<grades.length; i++) {
+              div.innerHTML +=
+              '<i style="background:' + grades[i] + '; color:' + grades[i] + '">A</i>     ' + Math.round(quantilesCalculated[i]) + '<br>';
+          }
+          return div;
+      };
+
+      if(regionWasSelected){
+        return {fillColor: getColor(currentValueReg, quantilesCalculated),
+                color: 'white', 
+                weight: 0.5}
+      } else {
+        return {fillColor: "gray",
+                color: 'white', 
+                weight: 1}
+      }
+    } else {
+      return {fillColor: "gray",
+              color: 'white', 
+              weight: 1}
+    }
+}
 
 //Manipulate size of the map
 function openNav() {
-  document.getElementById("graphsContainer").style.display = 'none'
-  document.getElementById("mapBox").style.width = "100vw";
-  document.getElementById("dimensionSelector").style.display = 'none'
 
-  setTimeout(function(){ 
-    wrapMap(regionDivision)
-    document.querySelector(".retractMap").style.display = 'block';
-    document.querySelector(".expandMap").style.display = 'none';
-  }, 300);
+  window.expanded = window.expanded == false;
+  console.log(window.expanded)
+
+  if(window.expanded == true){
+    document.getElementById("graphsContainer").style.display = 'none'
+    document.getElementById("mapBox").style.width = "100vw";
+    document.getElementById("mapBox").style.height = "76vh";
+
+    setTimeout(function(){ 
+      wrapMap(regionDivision, zoom + 0.35)
+    }, 300);
+
+    var expandMapDiv = document.querySelector('.expandMap')
+    expandMapDiv.innerHTML = "<"
+
+  } else {
+    document.getElementById("graphsContainer").style.display = ''
+    document.getElementById("mapBox").style.width = "20vw";
+    document.getElementById("mapBox").style.height = "";
+
+    setTimeout(function(){ 
+      wrapMap(regionDivision, zoom)
+    }, 300);
+
+  }
+
 }
 
 function closeNav() {
-  document.getElementById("graphsContainer").style.display = ''
-  document.getElementById("mapBox").style.width = "20vw";
-  document.getElementById("dimensionSelector").style.display = 'none'
-
-  setTimeout(function(){ 
-    wrapMap(regionDivision)
-    document.querySelector(".retractMap").style.display = 'none';
-    document.querySelector(".expandMap").style.display = 'block';
-  }, 300);
 }
 
+//Global variable indicating if map shoul or should not be presented in expanded form 
+var expanded = false;
+var showNames = false;
 
 //Show information about region
 function showProps(e, labels, regionDivision){
   if(e.target.feature.properties !== 'undefined'){
-    console.log(e.target.feature.properties[0][regionDivision])
     if(labels){
       var regionCode = e.target.feature.properties[0][regionDivision]
       var regionLabel = labels[0]['subLabels'][regionDivision][regionCode]
     } else {
       var regionLabel = e.target.feature.properties[0][regionDivision]
     }
-    console.log(e)
     info.update(regionLabel);
     //document.getElementById("showRegionHover").innerHTML = regionLabel
     //window.popup = L.popup({autoPan: false})
@@ -160,6 +265,15 @@ function onEachFeature(feature, layer) {
       click: function(feature){selectRegion(feature, regionDivision)}
       //mousemove: applyMousePositionToBox,
     });
+}
+
+function onEachFeatureWithNames(feature, layer) {
+    layer.on({
+      mouseover: function(feature){showProps(feature, labels, regionDivision)},
+      mouseout: closePopup,
+      click: function(feature){selectRegion(feature, regionDivision)}
+      //mousemove: applyMousePositionToBox,
+    });
     
     //Adding region names
     if(feature.properties){
@@ -173,7 +287,7 @@ function onEachFeature(feature, layer) {
     }
 }
 
-async function wrapMap(regionDivision){
+async function wrapMap(regionDivision, zoom){
   
   window.map.remove()
   window.map = L.map("mapBox", {zoomSnap: 0.1, crs: crs}).setView(centering, zoom);
@@ -184,7 +298,7 @@ async function wrapMap(regionDivision){
   var geoJSON = generateGeoJSON(geoModel, allFeatures)
   var geoJSON = assignValueToGeoJsonObject(geoJSON, window.data, regionDivision)
 
-  console.log(geoJSON)
+  //console.log(geoJSON)
 
   info.onAdd = function (map) {
       this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
@@ -196,28 +310,52 @@ async function wrapMap(regionDivision){
       this._div.innerHTML = (prop ? prop : '');
   };
   info.addTo(map);
-
-  L.geoJson(geoJSON, {
-      onEachFeature: onEachFeature,
-      style: styleGen,
-    }).addTo(map);
+  
+  if(window.expanded){
+    if(showNames){
+      L.geoJson(geoJSON, {
+          onEachFeature: onEachFeatureWithNames,
+          style: styleGenColors,
+        }).addTo(map);
+      window.legend.addTo(map);
+    } else {
+      L.geoJson(geoJSON, {
+          onEachFeature: onEachFeatureWithNames,
+          style: styleGen,
+        }).addTo(map);
+    }
+  } else {
+      L.geoJson(geoJSON, {
+          onEachFeature: onEachFeature,
+          style: styleGen,
+        }).addTo(map);
+  }
 
   //Creating map extension box
   var expandMapDiv = L.DomUtil.create('div', 'expandMap');
-  var retractMapDiv = L.DomUtil.create('div', 'retractMap');
+  var mapTypeDiv = L.DomUtil.create('div', 'mapType');
   var controlDivs = document.querySelector(".leaflet-control-zoom.leaflet-bar.leaflet-control")
 
   controlDivs.append(expandMapDiv);
-  controlDivs.append(retractMapDiv);
+  if(window.expanded){
+    controlDivs.append(mapTypeDiv);
+  }
 
-  expandMapDiv.innerHTML = ">"
-  retractMapDiv.innerHTML = "<"
+  mapTypeDiv.innerHTML = "i"
+
+  if(window.expanded){
+    expandMapDiv.innerHTML = "<"
+  } else {
+    expandMapDiv.innerHTML = ">"
+  }
 
   expandMapDiv.onclick = function(){
     openNav()    
   }
-  retractMapDiv.onclick = function(){
-    closeNav()    
+
+  mapTypeDiv.onclick = function(){
+    window.showNames = window.showNames == false;
+    wrapMap(regionDivision, zoom)
   }
 
 }
@@ -230,8 +368,6 @@ function fillMapSelection(checkedValues, whereAppend, labels, textTranslations){
   }
   document.getElementById(whereAppend).innerHTML = html
 }
-
-
 
 
 
